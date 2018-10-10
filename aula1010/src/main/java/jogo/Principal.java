4a5389e852eddca660dbba403643cf6890b7767a
Aluno <aluno@9cc73c92d689>
2018-10-10 11:40:40 -0300

    JDV

--- aula1010/src/main/java/jogo/Principal.java
+++ aula1010/src/main/java/jogo/Principal.java
@@ -1,8 +1,11 @@
 package jogo;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
 
 public class Principal {
     private JPanel painelPrincipal;
@@ -17,81 +20,156 @@ public class Principal {
     private JLabel Label22;
     private JLabel Jogador;
     private JButton iniciarButton;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField jogadorTF1;
+    private JTextField jogadorTF2;
+    private JRadioButton humanoRadioButton;
+    private JRadioButton computadorRadioButton;
+    private JRadioButton humanoRadioButton1;
+    private JRadioButton computadorRadioButton1;
+    private int iniciar = 0;
+    private String j1;
+    private String j2;
+
 
     public void troca(JLabel Labelxx,JLabel Jogador){
-        if(Jogador.getText().equals("Jogador 01")){
+        if(Jogador.getText().equals(this.j1)){
             Labelxx.setText("X");
-            Jogador.setText("Jogador 02");
+            Jogador.setText(this.j2);
         }
         else{
             Labelxx.setText("O");
-            Jogador.setText("Jogador 01");
+            Jogador.setText(this.j1);
         }
     }
+
+    public void limpar(){
+        Label00.setText("--");
+        Label01.setText("--");
+        Label02.setText("--");
+        Label10.setText("--");
+        Label11.setText("--");
+        Label12.setText("--");
+        Label20.setText("--");
+        Label21.setText("--");
+        Label22.setText("--");
+    }
+
+    public void inicio(){
+        this.iniciar=1;
+    }
+
+    public void nome(){
+        if(jogadorTF1.getText().equals("")){
+            this.j1="jogador1";
+        }
+        else{
+            this.j1=jogadorTF1.getText();
+        }
+        if(jogadorTF2.getText().equals("")){
+            this.j2="jogador2";
+        }
+        else{
+            this.j2=jogadorTF2.getText();
+        }
+    }
+
     public Principal() {
         Label00.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                troca(Label00, Jogador);
+                if(iniciar==1) {
+                    troca(Label00, Jogador);
+                }
             }
         });
         Label01.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                troca(Label01, Jogador);
+                if(iniciar==1) {
+                    troca(Label01, Jogador);
+                }
             }
         });
         Label02.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                troca(Label02, Jogador);
+                if(iniciar==1) {
+                    troca(Label02, Jogador);
+                }
             }
         });
         Label10.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                troca(Label10, Jogador);
+                if(iniciar==1) {
+                    troca(Label10, Jogador);
+                }
             }
         });
         Label11.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                troca(Label11, Jogador);
+                if(iniciar==1) {
+                    troca(Label11, Jogador);
+                }
             }
         });
         Label12.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                troca(Label12, Jogador);
+                if(iniciar==1) {
+                    troca(Label12, Jogador);
+                }
             }
         });
         Label20.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                troca(Label20, Jogador);
+                if(iniciar==1) {
+                    troca(Label20, Jogador);
+                }
             }
         });
         Label21.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                troca(Label21, Jogador);
+                if(iniciar==1) {
+                    troca(Label21, Jogador);
+                }
             }
         });
         Label22.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                troca(Label22, Jogador);
+                if(iniciar==1) {
+                    troca(Label22, Jogador);
+                }
+            }
+        });
+        iniciarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                limpar();
+                inicio();
+                nome();
+                Random r = new Random();
+                int i=r.nextInt(2)+1;
+                System.out.println(i);
+                if(i==1){
+                    Jogador.setText(j1);
+                }
+                else{
+                    Jogador.setText(j2);
+                }
             }
         });
     }
