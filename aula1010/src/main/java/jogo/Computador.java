b0b1036ab3e3079b85c70f661f45d681e50c4b03
Daniel Cabral Correa <daniel.liboos@gmail.com>
2018-10-29 15:47:34 -0300

    jogo

--- aula1010/src/main/java/jogo/Computador.java
+++ aula1010/src/main/java/jogo/Computador.java
@@ -1,7 +1,26 @@
 package jogo;
 
+import java.util.Random;
+
 public class Computador {
 
+    public int[][] posAleatorio(int[][] mat,Jogador jg){
+        int d = 0;
+        while(d==0) {
+            Random r = new Random();
+            int i = r.nextInt(8);
+            int linha = i / 3;
+            int coluna = i / 3;
+            if(mat[linha][coluna]==0){
+                mat[linha][coluna]= jg.getMarcador();
+                d=1;
+            }
+        }
+        return mat;
+    }
 
+    public int[][] inteligencia(int[][] mat,int valor){
+
+    }
 
 }
